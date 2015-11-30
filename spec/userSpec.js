5c972afe7df8b1fd5d3d6fe2332d00c1067b801c
Manuel Schnidrig <manuel.schnidrig@globant.com>
2015-11-30 17:26:58 -0300

    Especificación inicial de Usuario. Esbozo inicial objeto usuario

---
+++ spec/userSpec.js
@@ -0,0 +1,9 @@
+describe("User", function() {
+    it("should be able to register a user name and a password for the site", function() {
+        expect(true).toBe(true);
+    });
+    
+    it("should be able to log into the site", function () {
+        
+    });
+});
---
+++ src/app/User.js
@@ -0,0 +1,25 @@
+(function() {
+    
+    var _config = {
+        password: {
+            pattern: new RegExp(/^[a-z0-9]+$/i)
+        }
+    };
+    
+    var User = function (name, password) {
+        this.name = name;
+        this.password = this.setPassword(password);
+    }
+    
+    User.prototype = {
+        setPassword: function (password) {
+            if ( !_config.password.pattern.test(password) ) {
+                throw "Password only accepts alphanumeric characters.";
+            }
+        },
+        checkPassword: function (password) {
+            return this.password == password;
+        }
+    }
+    
+})();
