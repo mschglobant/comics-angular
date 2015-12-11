6869a800e386195d448447eba9ab1d79214e7307
Manuel Schnidrig <manuel.schnidrig@globant.com>
2015-12-11 10:57:07 -0300

    Paginación de comentarios

--- src/app/home/comics/comments/comments.directive.js
+++ src/app/home/comics/comments/comments.directive.js
@@ -22,15 +22,33 @@
 
   function CommentsController($scope) {
 
-    $scope.$watch('comic', function(comic) {
+    $scope.$watch('comic', function (comic) {
       if ($scope.comic) {
         $scope.comic.comments.forEach(function (comment) {
           if (comment.timestamp && (typeof comment.timestamp === 'string' || comment.timestamp instanceof String)) {
             comment.timestamp = new Date(comment.timestamp);
           }
         });
+
+        $scope.$watchCollection('comic.comments', function (comments) {
+
+        $scope.pagination.currentPage = Math.ceil(comic.comments.length / $scope.pagination.itemsPerPage);
+        });
+
       }
     });
+
+    $scope.pagination = {
+      currentPage: 1,
+      itemsPerPage: 5,
+      getCurrentElements: function (array) {
+        if (!array) return [];
+        var from = ($scope.pagination.currentPage-1)*$scope.pagination.itemsPerPage,
+            to = from+$scope.pagination.itemsPerPage;
+
+        return array.slice(from, to);
+      }
+    }
 
   }
 
--- src/app/home/comics/rate-card/rate-card.directive.js
+++ src/app/home/comics/rate-card/rate-card.directive.js
@@ -3,7 +3,6 @@
   'use strict';
 
   angular.module('comicsHome')
-    .controller('RateCardController', RateCardController)
     .directive('rateCard', rateCardDirective);
 
   // Directive ----------------------------------------------------------------
@@ -13,36 +12,9 @@
       restrict: 'E',
       replace: true,
       scope: {
-        id: '='
-      },
-      controller: 'RateCardController',
-      controllerAs: 'vm'
-    };
-  }
-
-  // Controller ---------------------------------------------------------------
-  RateCardController.$inject = ['$scope', 'comicsProvider'];
-
-  function RateCardController($scope, comicsProvider) {
-    var vm = this;
-
-    console.log($scope.id);
-
-    comicsProvider.findBy({
-      "id": $scope.comicId
-    }).then(function (comic) {
-      vm.comic = angular.copy(comic);
-
-      if (vm.comic.rate) {
-        var total = 0,
-            sum = .0;
-        for (var stars in vm.comic.rate.percentages) {
-          total = vm.comic.rate.percentages[stars];
-          sum += stars * total;
-        }
-        vm.comic.rate.avg = sum / total;
+        comic: '='
       }
-    });
+    };
   }
 
 }());
